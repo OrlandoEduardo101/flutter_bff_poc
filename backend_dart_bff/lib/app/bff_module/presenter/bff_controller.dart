7f1570168af047a1542adc47265daab09cac9867
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2022-11-16 19:08:07 -0300

    bff api

--- backend_dart_bff/lib/app/bff_module/presenter/bff_controller.dart
+++ backend_dart_bff/lib/app/bff_module/presenter/bff_controller.dart
@@ -5,10 +5,35 @@ class BffController {
   Future<Response> returnBffScreen(ModularArguments args, Request request) async {
     return Response.ok([
       {
-        'id_widget_list': [
-          'card1',
-          'card2',
-          'card3',
+        'widget_list': [
+          {
+            'id': 'card1',
+            'title': 'texto texto card 1',
+          },
+          {
+            'id': 'card2',
+            'title': 'texto texto card 2',
+          },
+          {
+            'id': 'card3',
+            'title': 'texto texto card 3',
+          },
+          {
+            'id': 'navButton',
+            'title': 'new blank page',
+            'route': '/blankPage',
+            'widget_list': [
+              {
+                'id': 'card1',
+                'title': 'texto texto card 1',
+              },
+              {
+                'id': 'navButton',
+                'title': 'homePage',
+                'route': '/blankPage',
+              },
+            ]
+          },
         ]
       }
     ]);
